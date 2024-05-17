e3fd2a57dbd082d5a399c129ef1dc31b4b99f598
JoYoungJinNode5 <jyj950406@naver.com>
2024-05-17 11:32:26 +0900

    폴더명 변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,21 +1,21 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const connect = () => {
   mongoose
     .connect(
-      "mongodb+srv://sparta-user:<password>@express-mongo.ncd0rot.mongodb.net/?retryWrites=true&w=majority&appName=express-mongo",
+      'mongodb+srv://sparta-user:<password>@express-mongo.ncd0rot.mongodb.net/?retryWrites=true&w=majority&appName=express-mongo',
       {
-        dbName: "sparta_mall",
+        dbName: 'sparta_mall',
       }
     )
-    .then(() => console.log("MongoDB 연결에 성공하였습니다."))
+    .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
     .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
 };
 
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB 연결 에러", err);
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB 연결 에러', err);
 });
 
 export default connect;
