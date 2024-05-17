2823f40b2becfa6b69ffe0a1088911ed297fcebe
JoYoungJinNode5 <jyj950406@naver.com>
2024-05-17 11:21:20 +0900

    인섬니아

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import express from "express";
-import connect from "./schemas/index.js";
-import productsRouter from "./routers/products.router.js";
+import express from 'express';
+import connect from './schemas/index.js';
+import productsRouter from 'routers/products.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -13,10 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 const router = express.Router();
 
-app.use("/", [router, productsRouter]);
+app.use('/', [router, productsRouter]);
 
 app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸어요!!");
+  console.log(PORT, '포트로 서버가 열렸어요!!');
 });
